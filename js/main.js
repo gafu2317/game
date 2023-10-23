ae435f244d297b2abb95b5c45d5cec2c26aeb275
OKU09 <o.mizuno.264@stn.nitech.ac.jp>
2023-10-23 12:50:40 +0900

    改変

--- js/main.js
+++ js/main.js
@@ -1,3 +1,69 @@
+// import Phaser from "phaser";
+// import Scene1 from "./picture-group.js";
+// import { create, preload, update } from "./picture-group";
+
+// const D_WIDTH = 1000;
+// const D_HEIGHT = 600;
+// // 1, Phaser3の設定データ
+// /**
+//  * @type {Phaser.Types.Core.GameConfig}
+//  */
+// export const config = {
+//   type: Phaser.AUTO,
+//   width: D_WIDTH, // ゲーム画面の横幅
+//   height: D_HEIGHT, // ゲーム画面の高さ
+//   antialias: false, // アンチエイリアスを無効にするらしい（分ってない）
+//   scene: Scene1,
+//   scene: {
+//     preload: preload, // 素材の読み込み時の関数
+//     create: create, // 画面が作られた時の関数
+//     update: update, // 連続実行される関数
+//   },
+
+//   fps: {
+//     target: 24, // フレームレート
+//     forceSetTimeOut: true,
+//   },
+//   physics: {
+//     default: "arcade",
+//     arcade: {
+//       debug: false, // スプライトに緑の枠を表示しない
+//       gravity: { y: 300 }, // 重力の方向とその強さ
+//     },
+//   },
+// };
+
+// new Phaser.Game(config);
+
+// function preload() {
+//   this.load.image("pin", "./img/pin.png");
+// }
+// function create() {
+//   // 背景色を設定
+//   this.cameras.main.setBackgroundColor("#50ffa2"); // 任意の色を指定
+//   // ボタンの作成
+//   const button1 = this.add
+//     .image(100, 500, "pin")
+//     .setInteractive()
+//     .setScale(0.3);
+//   const button2 = this.add.image(300, 500, "button2Texture").setInteractive();
+//   const button3 = this.add.image(500, 500, "button3Texture").setInteractive();
+
+//   // ボタンのクリックイベント
+//   button1.on("pointerdown", () => {
+//     this.scene.start("Scene1");
+//   });
+
+//   button2.on("pointerdown", () => {
+//     this.scene.start("Scene2");
+//   });
+
+//   button3.on("pointerdown", () => {
+//     this.scene.start("Scene3");
+//   });
+// }
+// function update() {}
+
 import Phaser from "phaser";
 import Scene1 from "./picture-group.js";
 import { create, preload, update } from "./picture-group";
@@ -34,32 +100,3 @@ export const config = {
 };
 
 new Phaser.Game(config);
-
-function preload() {
-  this.load.image("pin", "./img/pin.png");
-}
-function create() {
-  // 背景色を設定
-  this.cameras.main.setBackgroundColor("#50ffa2"); // 任意の色を指定
-  // ボタンの作成
-  const button1 = this.add
-    .image(100, 500, "pin")
-    .setInteractive()
-    .setScale(0.3);
-  const button2 = this.add.image(300, 500, "button2Texture").setInteractive();
-  const button3 = this.add.image(500, 500, "button3Texture").setInteractive();
-
-  // ボタンのクリックイベント
-  button1.on("pointerdown", () => {
-    this.scene.start("Scene1");
-  });
-
-  button2.on("pointerdown", () => {
-    this.scene.start("Scene2");
-  });
-
-  button3.on("pointerdown", () => {
-    this.scene.start("Scene3");
-  });
-}
-function update() {}
