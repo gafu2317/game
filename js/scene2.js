a089e80e99012c14f01e3f7ce13cfd968cdab2cc
gafu2317 <ryutaanubisu66@gmail.com>
2023-10-25 13:59:15 +0900

    背景表示、画像追加

--- js/scene2.js
+++ js/scene2.js
@@ -1,7 +1,7 @@
 /**
  * @type {Phaser.Types.Scenes.SettingsConfig}
  */
-export const scene1 = {
+export const scene2 = {
   preload: preload, // 素材の読み込み時の関数
   create: create, // 画面が作られた時の関数
   update: update, // 連続実行される関数
@@ -10,23 +10,33 @@ export const scene1 = {
 };
 
 function preload(){
-  this.load.image("", "/img/tower/tower.png");
-  this.load.image("", "/img/tower/tower2.png");
-  this.load.image("", "/img/tower/towerBack.png");
-  this.load.image("", "/img/tower/towerWall2.png");
-  this.load.image("", "/img/tower/BlackItem.png");
-  this.load.image("", "/img/tower/WhiteItem.png");
-  this.load.image("", "/img/tower/gun.png");
-  this.load.image("", "/img/tower/stick.png");
-  this.load.image("", "/img/tower/sword.png");
-  this.load.image("", "/img/tower/poison.png");
-  this.load.image("", "/img/tower/dragon.png");
-  this.load.image("", "/img/tower/golem.png");
-  this.load.image("", "/img/tower/slime.png");
+  // this.load.image("", "/img/tower/tower.png");タワーの画像（ボツ）
+  this.load.image("tower", "/img/tower/tower2.png");
+  this.load.image("inner-tower", "/img/tower/towerWall2.png");
+  this.load.image("background", "/img/tower/towerBack10-6.jpg");
+  //hはhealの略、bはbuffの略、dはdebuffの略
+  this.load.image("item-h-black", "/img/tower/BlackItem.png");
+  this.load.image("item-h-white", "/img/tower/WhiteItem.png");
+  this.load.image("item-b-gun", "/img/tower/gun.png");
+  this.load.image("item-b-stick", "/img/tower/stick.png");
+  this.load.image("item-b-sword", "/img/tower/sword.png");
+  this.load.image("item-d-poison", "/img/tower/poison.png");
+  this.load.image("enemy-dragon", "/img/tower/dragon.png");
+  this.load.image("enemy-golem", "/img/tower/golem.png");
+  this.load.image("enemy-sline", "/img/tower/slime.png");
+
+  this.load.spritesheet("human", "/img/tower/human.png", {
+    frameWidth: 146, // 1フレームの幅
+    frameHeight: 286, // 1フレームの高さ
+  })
 
 }
 
 function create(){
+  const background = this.add.image(500, 300, "background");
+  background.setDisplaySize(1000, 600);
+
+  const tower = this.add.image(500, 300, "tower");
 
 }
 
